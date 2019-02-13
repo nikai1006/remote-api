787f6bd76590875c9bb9d8c35e97fdb057d67e70
nikai <keni1006@163.com>
2019-02-14 01:11:32 +0800

    add service

---
+++ src/main/java/cn/net/nikai/vue/remoteapi/controller/VueController.java
@@ -0,0 +1,17 @@
+package cn.net.nikai.vue.remoteapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * remote-api cn.net.nikai.vue.remoteapi.controller
+ *
+ * @author: nikai
+ * @Description:
+ * @Date: Create in 0:58 2019/2/14
+ * @Modified By:
+ */
+@RestController
+public class VueController {
+
+
+}
