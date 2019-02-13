613a050f57fbad4be00f16e4b6a91d45659cac2f
nikai <keni1006@163.com>
2019-02-14 00:55:09 +0800

    init project

---
+++ src/main/java/cn/net/nikai/vue/remoteapi/RemoteApiApplication.java
@@ -0,0 +1,14 @@
+package cn.net.nikai.vue.remoteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteApiApplication.class, args);
+	}
+
+}
+
